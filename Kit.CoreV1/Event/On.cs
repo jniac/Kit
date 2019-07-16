9f04abdefd4c6b27a10ba44377e3fb7c573f8788
jniac <joseph.merdrignac@gmail.com>
2019-07-16 09:05:02 +0200

    [up] ON() overloads

--- Kit.CoreV1/Event/On.cs
+++ Kit.CoreV1/Event/On.cs
@@ -3,12 +3,19 @@ namespace Kit.CoreV1
 {
     public partial class Event
     {
-        public static Listener On(object target, object type, 
-            Action<Event> callback = null, 
-            Action<Event> enter = null, 
-            Action<Event> exit = null, 
+		public static Listener On(object target, object type,
+            Action<Event> callback = null,
+            Action<Event> enter = null,
+            Action<Event> exit = null,
             object key = null)
             => new Listener(target, type, key, callback, enter, exit, typeof(Event));
+
+        public static Listener On(object target,
+            Action<Event> callback = null,
+            Action<Event> enter = null,
+            Action<Event> exit = null,
+            object key = null)
+            => new Listener(target, "*", key, callback, enter, exit, typeof(Event));
 
         public static Listener On<T>(object target, object type,
             Action<T> callback = null,
@@ -30,6 +37,14 @@ namespace Kit.CoreV1
 
             return new Listener(target, type, key, callbackT, enterT, exitT, typeof(T));
         }
+
+        public static Listener On<T>(object target,
+            Action<T> callback = null,
+            Action<T> enter = null,
+            Action<T> exit = null,
+            object key = null)
+            where T : Event
+            => On<T>(target, "*", callback, enter, exit, key);
 
         public static Listener On<T>(
             Action<T> callback = null,
