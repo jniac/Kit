a998cffb4172c17cedafbacf869b421d25ee4128
jniac <joseph.merdrignac@gmail.com>
2019-07-17 09:59:30 +0200

    [add] IDictionaryExtensions

---
+++ Kit.Utils/IDictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kit.Utils
+{
+    public static class IDictionaryExtensions
+    {
+        public static void Set<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key] = value;
+            }
+            else
+            {
+                dictionary.Add(key, value);
+            }
+        }
+    }
+}
