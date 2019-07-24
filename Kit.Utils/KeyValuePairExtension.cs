a702fa4e347fb103dea18fe5a55fa7482c4b3a03
jniac <joseph.merdrignac@gmail.com>
2019-07-24 10:59:21 +0200

    [add] Deconstruct KeyValuePair

---
+++ Kit.Utils/KeyValuePairExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kit.Utils
+{
+    public static class KeyValuePairExtension
+    {
+        // ex:
+        // Dictionary<string, float> dict = new Dictionary<string, float> { ["One"] = 1, ["Two"] = 2 };
+        // foreach (var (key, value) in dict) 
+        // { ... }
+        public static void Deconstruct<T, U>(this KeyValuePair<T, U> entry, out T key, out U value)
+        {
+            key = entry.Key;
+            value = entry.Value;
+        }
+    }
+}
