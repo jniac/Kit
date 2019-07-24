d61b3eede54f2d04a76c51d8ba41e9dd5a825b62
jniac <joseph.merdrignac@gmail.com>
2019-07-24 11:05:49 +0200

    [add] async Wait<Event>()

---
+++ Kit.CoreV1/Event/Wait.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kit.CoreV1
+{
+    public partial class Event
+    {
+        static int sleepDuration = 10;
+
+        public static async Task<T> Wait<T>(object target = null, object type = null)
+            where T : class, IEvent
+        {
+            T e = null;
+
+            Once<T>(target, type, e2 => e = e2);
+
+            await Task.Run(() =>
+            {
+                while (e == null)
+                    System.Threading.Thread.Sleep(sleepDuration);
+            });
+
+            return e;
+        }
+    }
+}
