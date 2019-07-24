5039e3eed702199f06740d12937fd8c85890a6a3
jniac <joseph.merdrignac@gmail.com>
2019-07-24 18:27:07 +0200

    [add] IEnumerable Ext: ForInterval

--- Kit.Utils/IEnumerableExtensions.cs
+++ Kit.Utils/IEnumerableExtensions.cs
@@ -66,5 +66,47 @@ namespace Kit.Utils
         public static TItem MaxItem<TItem, TValue>(this IEnumerable<TItem> items, Func<TItem, TValue> itemToValue)
             where TValue : IComparable
             => Extremum(items, itemToValue, false).item;
+
+
+
+
+        /*
+         * Interval loop, eg:
+         * [a, b, c, d].ForInterval(interval => {
+         *      var (item0, item1, index, total) = interval;       
+         * })
+         */
+        public static IEnumerable<T> ForInterval<T>(this IEnumerable<T> items,
+            Action<(T itemA, T itemB, int index, int total)> callback)
+        {
+            int count = 0;
+            int total = items.Count() - 1;
+            T previous = default;
+
+            foreach (T item in items)
+            {
+                if (count > 0)
+                    callback((previous, item, count - 1, total));
+
+                previous = item;
+                count++;
+            }
+
+            return items;
+        }
+        public static IEnumerable<T> ForInterval<T>(this IEnumerable<T> items,
+            Action<T, T> callback)
+            => ForInterval(items, i => callback(i.itemA, i.itemB));
+
+        /*
+         * useful for distance calculation, eg:
+         * total = [a, b, c, d].ReduceInterval((d, p0, p1) => d + distance(p1 - p0), 0f)
+         */
+        public static U ReduceInterval<T, U>(this IEnumerable<T> items, Func<U, T, T, U> callback, U start = default)
+        {
+            items.ForInterval((a, b) => start = callback(start, a, b));
+
+            return start;
+        }
     }
 }
