3d741d368b11cd6e4c8f56f809497bbda5bc82d2
jniac <joseph.merdrignac@gmail.com>
2019-07-25 00:27:48 +0200

    [new] Mth!

---
+++ Kit.Utils/Mth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kit.Utils
+{
+    public static class Mth
+    {
+        public static float Clamp(float x) => x < 0 ? 0 : x > 1 ? 1 : x;
+
+        public static float Clamp(float x, float min, float max) => x < min ? min : x > max ? max : x;
+
+        public static int Clamp(int x) => x < 0 ? 0 : x > 1 ? 1 : x;
+
+        public static int Clamp(int x, int min, int max) => x < min ? min : x > max ? max : x;
+    }
+}
