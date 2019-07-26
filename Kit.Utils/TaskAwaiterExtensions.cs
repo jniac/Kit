e14e5a53e2bc0ab033cfb6cd9e483178f700bbbf
jniac <joseph.merdrignac@gmail.com>
2019-07-26 14:53:40 +0200

    [add] await anything!

---
+++ Kit.Utils/TaskAwaiterExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Kit.Utils
+{
+    // inspired from https://devblogs.microsoft.com/pfxteam/await-anything/
+    public static class TaskAwaiterExtensions
+    {
+        public static TaskAwaiter GetAwaiter(this TimeSpan timeSpan) =>
+            Task.Delay(timeSpan).GetAwaiter();
+
+        public static TaskAwaiter GetAwaiter(this float seconds) => 
+            TimeSpan.FromSeconds(seconds).GetAwaiter();
+    }
+}
