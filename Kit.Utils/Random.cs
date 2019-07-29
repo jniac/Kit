df7c6065bf09b8ea1ce61b52099007904607d658
jniac <joseph.merdrignac@gmail.com>
2019-07-29 13:40:29 +0200

    [new] Random

---
+++ Kit.Utils/Random.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Kit.Utils
+{
+    public class Random
+    {
+        public static Random R = new Random();
+
+        System.Random random;
+
+        const float INT_MAX_VALUE = int.MaxValue;
+
+        public Random(int seed = 12345)
+        {
+            random = new System.Random(seed);
+        }
+
+        public float Float() => random.Next() / INT_MAX_VALUE;
+        public float Float(float max) => max * Float();
+        public float Float(float min, float max) => min + (max - min) * Float();
+    }
+}
