20474fbc97b3153fb4cc525db23e3c8b00154618
jniac <joseph.merdrignac@gmail.com>
2019-07-30 11:29:40 +0200

    [add] StopWatch Ext (microseconds, nanoseconds)

---
+++ Kit.Utils/StopWatchExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Kit.Utils
+{
+    public static class StopWatchExtensions
+    {
+        public static int GetElapsedMilliSeconds(this Stopwatch stopWatch)
+            => (int)(1e3f * stopWatch.ElapsedTicks / Stopwatch.Frequency);
+
+        public static int GetElapsedMicroSeconds(this Stopwatch stopWatch)
+            => (int)(1e6f * stopWatch.ElapsedTicks / Stopwatch.Frequency);
+
+        public static int GetElapsedNanoSeconds(this Stopwatch stopWatch)
+            => (int)(1e9f * stopWatch.ElapsedTicks / Stopwatch.Frequency);
+    }
+}
