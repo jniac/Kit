cdf371893b0219ed083b5eea9263b5748969fcf8
jniac <joseph.merdrignac@gmail.com>
2019-09-15 23:58:47 +0200

    [add] Kit.Utils: FloatAverage/TimeMonitor

---
+++ Kit.Utils/FloatAverage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Kit.Utils
+{
+    public class FloatAverage
+    {
+        int index = 0;
+        int length = 10;
+        float[] values;
+
+        float sum;
+
+        public FloatAverage(int length)
+        {
+            this.length = length;
+            values = new float[length];
+            sum = 0;
+        }
+
+        public FloatAverage() : this(10) { }
+
+        public float Average => sum / length;
+
+        public void SetCurrentValue(float value)
+        {
+            sum += -values[index] + value;
+            values[index] = value;
+
+            index = index + 1 < length ? index + 1 : 0;
+        }
+
+        public float CurrentValue
+        {
+            get => values[index];
+            set => SetCurrentValue(value);
+        }
+    }
+}
---
+++ Kit.Utils/TimeMonitor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Kit.Utils
+{
+    public static class TimeMonitor
+    {
+        static Dictionary<object, FloatAverage> dictionary = new Dictionary<object, FloatAverage>();
+        static Stopwatch watch = new Stopwatch();
+
+        public static float Monitor(object key, Action action)
+        {
+            watch.Restart();
+
+            action();
+
+            watch.Stop();
+
+            if (!dictionary.ContainsKey(key))
+                dictionary.Add(key, new FloatAverage());
+
+            FloatAverage floatAverage = dictionary[key];
+
+            floatAverage.CurrentValue = watch.GetElapsedMicroSeconds();
+
+            return floatAverage.CurrentValue;
+        }
+
+        public static float GetAverage(object key)
+        {
+            if (dictionary.TryGetValue(key, out FloatAverage floatAverage))
+                return floatAverage.Average;
+
+            return -1;
+        }
+
+        public static bool Clear(object key)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary.Remove(key);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
