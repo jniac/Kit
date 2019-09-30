a55a4e72674ab38b95c58f3b7ae365c3161add24
jniac <joseph.merdrignac@gmail.com>
2019-09-30 21:35:22 +0200

    [add] Mth.Mix(a, b, t), Mth.Limited(x, max)

--- Kit.Utils/Mth.cs
+++ Kit.Utils/Mth.cs
@@ -11,5 +11,10 @@ namespace Kit.Utils
         public static int Clamp(int x) => x < 0 ? 0 : x > 1 ? 1 : x;
 
         public static int Clamp(int x, int min, int max) => x < min ? min : x > max ? max : x;
+
+        public static float Mix(float a, float b, float t, bool clamp = true) => a + (b - a) * (clamp ? Clamp(t) : t);
+
+        // https://www.desmos.com/calculator/5qrnsfomsl
+        public static float Limited(float x, float max) => (max * x) / (max + x);
     }
 }
