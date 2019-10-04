8a0e218a4ee3bcde3362f4f258054d1bf9176766
jniac <joseph.merdrignac@gmail.com>
2019-10-05 00:25:40 +0200

    [rfctr] Select: layer Enter|Exit overloads

--- Kit.CoreV1/Select/SelectOverload.cs
+++ Kit.CoreV1/Select/SelectOverload.cs
@@ -8,35 +8,47 @@ namespace Kit.CoreV1
         // Get: Generic
         public IEnumerable<T> Get<TLayer>() =>
             GetLayer<TLayer>().set;
+
+
+
         // Enter: layer
         public void Enter(object layerKey, T item)
             => GetLayer(layerKey).Enter(item);
 
+        public void EnterAll(object layerKey, IEnumerable<T> items)
+            => GetLayer(layerKey).EnterAll(items);
+
+        public void EnterAll(object layerKey)
+            => GetLayer(layerKey).EnterAll();
+
         public void Enter(object layerKey, params T[] items)
             => GetLayer(layerKey).Enter(items);
-
-        public void EnterAll(object layerKey, T item)
-            => GetLayer(layerKey).EnterAll();
 
         // Enter: default
         public void Enter(T item)
             => GetLayer(defaultLayerKey).Enter(item);
 
-        public void Enter(params T[] items)
-            => GetLayer(defaultLayerKey).Enter(items);
+        public void EnterAll(IEnumerable<T> items)
+            => GetLayer(defaultLayerKey).EnterAll(items);
 
         public void EnterAll()
             => GetLayer(defaultLayerKey).EnterAll();
+
+        public void Enter(params T[] items)
+            => GetLayer(defaultLayerKey).Enter(items);
 
         // Enter: generic
         public void Enter<TLayer>(T item)
             => GetLayer<TLayer>().Enter(item);
 
-        public void Enter<TLayer>(T[] items)
-            => GetLayer<TLayer>().Enter(items);
+        public void EnterAll<TLayer>(IEnumerable<T> items)
+            => GetLayer<TLayer>().EnterAll(items);
 
         public void EnterAll<TLayer>()
             => GetLayer<TLayer>().EnterAll();
+
+        public void Enter<TLayer>(T[] items)
+            => GetLayer<TLayer>().Enter(items);
 
 
 
@@ -44,31 +56,40 @@ namespace Kit.CoreV1
         public void Exit(object layerKey, T item)
             => GetLayer(layerKey).Exit(item);
 
+        public void ExitAll(object layerKey, IEnumerable<T> items)
+            => GetLayer(layerKey).ExitAll(items);
+
+        public void ExitAll(object layerKey)
+            => GetLayer(layerKey).ExitAll();
+
         public void Exit(object layerKey, params T[] items)
             => GetLayer(layerKey).Exit(items);
-
-        public void ExitAll(object layerKey, T item)
-            => GetLayer(layerKey).ExitAll();
 
         // Exit: default
         public void Exit(T item)
             => GetLayer(defaultLayerKey).Exit(item);
 
-        public void Exit(params T[] items)
-            => GetLayer(defaultLayerKey).Exit(items);
+        public void ExitAll(IEnumerable<T> items)
+            => GetLayer(defaultLayerKey).ExitAll(items);
 
         public void ExitAll()
             => GetLayer(defaultLayerKey).ExitAll();
+
+        public void Exit(params T[] items)
+            => GetLayer(defaultLayerKey).Exit(items);
 
         // Exit: generic
         public void Exit<TLayer>(T item)
             => GetLayer<TLayer>().Exit(item);
 
-        public void Exit<TLayer>(T[] items)
-            => GetLayer<TLayer>().Exit(items);
+        public void ExitAll<TLayer>(IEnumerable<T> items)
+            => GetLayer<TLayer>().ExitAll(items);
 
         public void ExitAll<TLayer>()
             => GetLayer<TLayer>().ExitAll();
+
+        public void Exit<TLayer>(T[] items)
+            => GetLayer<TLayer>().Exit(items);
 
 
 
