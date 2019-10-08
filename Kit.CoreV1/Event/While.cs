fa6efd012508dd711f0459b7d8e3eca740fa315d
jniac <joseph.merdrignac@gmail.com>
2019-10-08 17:53:13 +0200

    [add] Event: WhileNot<T>()

--- Kit.CoreV1/Event/While.cs
+++ Kit.CoreV1/Event/While.cs
@@ -11,8 +11,8 @@ namespace Kit.CoreV1
             Listener master = null;
 
             master = On<T>(target, type,
-                enter:e => master.ChildrenDisabled = false, 
-                exit:e => master.ChildrenDisabled = true);
+                enter: e => master.ChildrenDisabled = false,
+                exit: e => master.ChildrenDisabled = true);
 
             master.IsWhileContainer = true;
             master.ChildrenDisabled = true;
@@ -30,5 +30,34 @@ namespace Kit.CoreV1
         public static Listener While<T>(params Listener[] listeners)
             where T : class, IEvent
             => While<T>(global, "*", listeners);
+
+
+
+        public static Listener WhileNot<T>(object target, object type, params Listener[] listeners)
+            where T : class, IEvent
+        {
+            Listener master = null;
+
+            master = On<T>(target, type,
+                enter: e => master.ChildrenDisabled = true,
+                exit: e => master.ChildrenDisabled = false);
+
+            master.IsWhileContainer = true;
+            master.ChildrenDisabled = false;
+
+            foreach (var listener in listeners)
+                master.AddChild(listener);
+
+            return master;
+        }
+
+        public static Listener WhileNot<T>(object target, params Listener[] listeners)
+            where T : class, IEvent
+            => WhileNot<T>(target, "*", listeners);
+
+        public static Listener WhileNot<T>(params Listener[] listeners)
+            where T : class, IEvent
+            => WhileNot<T>(global, "*", listeners);
+
     }
 }
