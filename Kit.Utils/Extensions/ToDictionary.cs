1e65d5164889c9c65851842a3602ba784211e06f
jniac <joseph.merdrignac@gmail.com>
2019-10-18 10:58:29 +0200

    [add] IEnumerable<T>: new Extensions (new way of declaration)

---
+++ Kit.Utils/Extensions/ToDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kit.Utils
+{
+    public static class ToDictionaryExtensions
+    {
+        public static IDictionary<T, TValue> ToDictionary<T, TValue>(this IEnumerable<T> items, Func<T, int, TValue> valueDelegate)
+        {
+            int index = 0;
+
+            return items.ToDictionary(value => value, value => valueDelegate(value, index++));
+        }
+    }
+}
---
+++ Kit.Utils/Extensions/ToList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kit.Utils
+{
+    public static class ToListExtensions
+    {
+        public static IList<TValue> ToList<T, TValue>(this IEnumerable<T> items, Func<T, TValue> valueDelegate)
+        {
+            return items.Select(value => valueDelegate(value)).ToList();
+        }
+
+        public static IList<TValue> ToList<T, TValue>(this IEnumerable<T> items, Func<T, int, TValue> valueDelegate)
+        {
+            int index = 0;
+
+            return items.Select(value => valueDelegate(value, index++)).ToList();
+        }
+    }
+}
