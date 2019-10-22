bd33179896a5818916eea2c7f70ff178c33be5fb
jniac <joseph.merdrignac@gmail.com>
2019-10-22 16:38:36 +0200

    [rfctr] Await ext file (explicit name)

--- Kit.Utils/Extensions/Await.cs
+++ Kit.Utils/Extensions/Await.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace Kit.Utils
 {
     // inspired from https://devblogs.microsoft.com/pfxteam/await-anything/
-    public static class TaskAwaiterExtensions
+    public static class Await
     {
         public static TaskAwaiter GetAwaiter(this TimeSpan timeSpan) =>
             Task.Delay(timeSpan).GetAwaiter();
