74427a34e9e3a587dc6a94b2e918541e7848c08c
jniac <joseph.merdrignac@gmail.com>
2019-10-25 22:14:29 +0200

    [fix] Wait leak fixed: On > Once!

--- Event/Wait.cs
+++ Event/Wait.cs
@@ -7,10 +7,10 @@ namespace Kit
     public partial class Event
     {
         public static Listener.InvocationAwaitable Wait(object target = null, object type = null, object key = null, int priority = PRIORITY_NORMAL)
-            => On(target, type, key: key, priority: priority).Invocation;
+            => Once(target, type, key: key, priority: priority).Invocation;
 
         public static Listener<T>.InvocationAwaitable Wait<T>(object target = null, object type = null, object key = null, int priority = PRIORITY_NORMAL)
             where T : class, IEvent
-            => On<T>(target, type, key: key, priority: priority).Invocation;
+            => Once<T>(target, type, key: key, priority: priority).Invocation;
     }
 }
