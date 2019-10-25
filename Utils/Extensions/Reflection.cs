7ef40b4abcce75ef66851675e88458291ce32558
jniac <joseph.merdrignac@gmail.com>
2019-10-25 14:24:56 +0200

    [add] Reflection ext: IsStatic()

---
+++ Utils/Extensions/Reflection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Kit
+{
+    public static class ReflectionExtensions
+    {
+        public static bool IsStatic(this PropertyInfo source, bool nonPublic = false)
+            => source.GetAccessors(nonPublic).Any(x => x.IsStatic);
+    }
+
+}
